dd2af2a828dbfbcf5eb8a7035a56aa773679aef7
vu22007 <123551420+vu22007@users.noreply.github.com>
2024-11-02 19:06:48 +0000

    Pause menu and game over screen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -8,6 +10,7 @@ public class GameController : MonoBehaviour
     [SerializeField] Shop shop;
     [SerializeField] GameObject pauseMenu;
     [SerializeField] GameObject gameOverScreen;
+    [SerializeField] TextMeshProUGUI highscoreText;
     List<Enemy> enemies;
     int highscore;
     int currentWave;
@@ -54,14 +57,20 @@ public class GameController : MonoBehaviour
 
     void GameOver(){
         running = false;
+        UpdateGameOverText();
         gameOverScreen.SetActive(true);
     }
 
     public void NewGame(){
+        gameOverScreen.SetActive(false);
         player.PlayerStart();
         currentWave = 0;
         enemies = new List<Enemy>();
         NewWave();
+    }
+
+    public void Quit(){
+        Application.Quit();
     }
 
     void NewWave(){
@@ -81,6 +90,10 @@ public class GameController : MonoBehaviour
         if (currentWave > highscore){
             highscore = currentWave;
         }
+    }
+
+    void UpdateGameOverText(){
+        highscoreText.text = "you did " + currentWave +" waves, the highscore is " + highscore;
     }
 
     Vector3 GenerateSpawnLocation(){
