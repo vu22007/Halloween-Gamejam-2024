9bbca0e2b41844511f19fe0e9651258938f7674c
Dhillon <dhillon.thu@gmail.com>
2024-11-02 12:50:06 +0000

    Added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,19 +3,22 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
-    float speed;
+    public float speed;
     int health;
     int maxHealth;
     int damage;
     int coinsDrop;
 
     public void EnemyMovement(Vector3 playerLocation){
-        
+        //just go towards the player
+        Vector3 enemyPos = gameObject.transform.position;
+        Vector3 relativePosition = playerLocation - enemyPos;
+        Vector3 movement = relativePosition.normalized * speed * Time.deltaTime;
+        gameObject.transform.position += movement;
     }
-
     public void TakeDamage(int damage){
         health -= damage;
-        if (DeadCheck()){
+                if (DeadCheck()){
             //spawn coins equal to coinsDrop
         }
     }
