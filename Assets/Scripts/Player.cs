9d32052f2370f2d284ddc43fcb71311c2c05edfb
yh22668 <yh22668@bristol.ac.uk>
2024-11-02 12:09:49 +0000

    Added movement functions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,27 @@ public class Player : MonoBehaviour
     int maxHealth;
     float speed;
     int money;
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
 
-    public void PlayerMovement(){
-        //use speed
+    void Start (){
+        body = GetComponent<Rigidbody2D>(); 
     }
-    
+
+    void Update (){
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical"); 
+    }
+
+    private void FixedUpdate(){
+        if (horizontal != 0 && vertical != 0) 
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+
     void TakeDamage(int damage){
         health -= damage;
         if (GameOverCheck()){
