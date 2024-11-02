20514697373e8d34a9b119a044676e02c4f5ac6d
vu22007 <123551420+vu22007@users.noreply.github.com>
2024-11-02 11:44:46 +0000

    player script template

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    int health;
+    int maxHealth;
+    float speed;
+    int money;
+
+    void PlayerMovement(){
+        //use speed
+    }
+    void TakeDamage(int damage){
+        health -= damage;
+        if (GameOverCheck()){
+            //game over stuff
+        }
+    }
+
+    void GetMoney(int amount){
+        money += amount;
+    }
+
+    bool GameOverCheck(){
+        if(health <= 0){
+            return true;
+        }
+        return false;
+    }
+}
