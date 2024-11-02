f5c26defcbf37d966d1f92926590afb746fe3309
yh22668 <yh22668@bristol.ac.uk>
2024-11-02 16:40:34 +0000

    Adding bullet prefab

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class Bullet
 {
-    
+    Vector3 direction;
+    float speed;
+    Rigidbody2D body;
+
+    public void OnCreate(Vector3 startDirection){
+      direction = startDirection;
+    }
+
+    public void BulletUpdate(){
+      body.linearVelocity = direction * speed;
+    }
 }
