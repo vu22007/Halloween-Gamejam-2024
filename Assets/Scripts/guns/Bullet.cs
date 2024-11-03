d60f6be44fc3ab039d463b5b2f9592c4d771cb63
yh22668 <yh22668@bristol.ac.uk>
2024-11-03 11:35:55 +0000

    Fixed bullet movement

--- Assets/Scripts/guns/Bullet.cs
+++ Assets/Scripts/guns/Bullet.cs
@@ -1,18 +1,17 @@
 using UnityEngine;
-using UnityEngine.Pool;
 
 public class Bullet : MonoBehaviour
 {
     Rigidbody2D body;
     Vector3 velocity;
     float health;
-    float speed;
 
     public void OnCreated(Vector3 startDirection, float bulletHealth, float speed){
         body = GetComponent<Rigidbody2D>();
         velocity = startDirection * speed;
         velocity.z = 0;
         health = bulletHealth;
+        Debug.Log(velocity);
     }
 
     void OnTriggerEnter2D(Collider2D other) {
